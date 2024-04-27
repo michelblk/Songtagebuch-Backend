bc3d7d5d47071dcadff5fe09af5853bd08aa91a7
Michel <git@michelblank.de>
2024-04-27 18:48:39 +0200

    Init project

---
+++ src/main/java/de/michelblank/songtagebuch/SongtagebuchApplication.java
@@ -0,0 +1,13 @@
+package de.michelblank.songtagebuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongtagebuchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongtagebuchApplication.class, args);
+	}
+
+}
